903307d4b455b3bb2bab49ee96b0c14a5d3c3210
박범진 <@DESKTOP-LDR8D7J>
2022-04-01 17:00:48 +0900

    [박범진] Staff.java

---
+++ sakila/src/main/java/vo/Staff.java
@@ -0,0 +1,76 @@
+package vo;
+
+public class Staff { 
+	// picture 제외
+	private int staffId; // 기본키
+	private String staffName;
+	private int addressId; // 외래키
+	private String staffAddress;
+	private String email;
+	private int storeId; // 외래키
+	private int active;
+	private String userName;
+	private String password;
+	private String lastUpdate;
+	
+	public int getStaffId() {
+		return staffId;
+	}
+	public void setStaffId(int staffId) {
+		this.staffId = staffId;
+	}
+	public String getStaffName() {
+		return staffName;
+	}
+	public void setStaffName(String staffName) {
+		this.staffName = staffName;
+	}
+	public int getAddressId() {
+		return addressId;
+	}
+	public void setAddressId(int addressId) {
+		this.addressId = addressId;
+	}
+	public String getStaffAddress() {
+		return staffAddress;
+	}
+	public void setStaffAddress(String staffAddress) {
+		this.staffAddress = staffAddress;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public int getStoreId() {
+		return storeId;
+	}
+	public void setStoreId(int storeId) {
+		storeId = storeId;
+	}
+	public int getActive() {
+		return active;
+	}
+	public void setActive(int active) {
+		this.active = active;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String username) {
+		this.userName = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getLastUpdate() {
+		return lastUpdate;
+	}
+	public void setLastUpdate(String lastUpdate) {
+		this.lastUpdate = lastUpdate;
+	}
+}
