2bdf66ce9e561e2c185fb9cb370f4cb730f38c7e
박범진 <@DESKTOP-LDR8D7J>
2022-04-04 16:12:47 +0900

    [박범진] Film.java, FilmDao.java, filmList.jsp

---
+++ sakila/src/main/java/dao/FilmDao.java
@@ -0,0 +1,96 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import util.DBUtil;
+import vo.Film;
+
+public class FilmDao {
+	
+	public List<Film> selectFilmListByPage(int beginRow, int rowPerPage) {
+		List<Film> list = new ArrayList<Film>();
+		
+		// DB 초기화
+		Connection conn = null;
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		
+		// static으로 선언해서 ~
+		// MariaDB 드라이버 로딩 메서드 호출
+		conn = DBUtil.getConnection();
+		String sql = 
+			"SELECT FID filmId, title title, description description, category name, price rentalRate, length length, rating rating, actors actor FROM film_list ORDER BY FID limit ?,?";
+	
+		try {
+			// SQL 쿼리 저장, 페이증 처리
+			stmt = conn.prepareStatement(sql);
+			stmt.setInt(1, beginRow);
+			stmt.setInt(2, rowPerPage);
+			rs = stmt.executeQuery();
+			// 데이터 변환
+			while(rs.next()) {
+				Film f = new Film(); // 다형성 
+				f.setFilmId(rs.getInt("filmId"));
+				f.setTitle(rs.getString("title"));
+				f.setDescription(rs.getString("description"));
+				f.setName(rs.getString("name"));
+				f.setRentalRate(rs.getInt("rentalRate"));
+				f.setLength(rs.getInt("length"));
+				f.setRating(rs.getString("rating"));
+				f.setActor(rs.getString("actor"));
+				list.add(f);
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				// DB자원반환
+				rs.close();
+				stmt.close(); 
+				conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return list;
+	}
+	
+	public int selectFilmTotalRow() throws Exception {
+		int row = 0; // 함수 결과값(쩡수) 반환해줄 변수 선언 후 초기화
+		
+		// DB 초기화
+		Connection conn = null;
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		
+		// static으로 선언해서 ~
+		conn = DBUtil.getConnection();
+		String sql = "SELECT COUNT(*) cnt FROM film_list";
+		
+		try {
+			stmt = conn.prepareStatement(sql);
+			rs = stmt.executeQuery();
+			if(rs.next()) {
+				row = rs.getInt("cnt");
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				//DB자원반납
+				rs.close();
+				stmt.close();
+				conn.close();
+			} catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return row;
+	}
+}
---
+++ sakila/src/main/java/vo/Film.java
@@ -0,0 +1,65 @@
+package vo;
+
+public class Film {
+	private int filmId; // film_id AS FID
+	private String title;
+	private String description;
+	private String name; // name AS category
+	private int rentalRate; // rental_rate AS price
+	private int length; // 
+	private String rating; 
+	private String actor; // GROUP_CONCAT(CONCAT(actor.first_name, _utf8mb4' ', actor.last_name) SEPARATOR ', ') 
+	@Override
+	public String toString() {
+		return "Film [filmId=" + filmId + ", title=" + title + ", description=" + description + ", name=" + name
+				+ ", rentalRate=" + rentalRate + ", length=" + length + ", rating=" + rating + ", actor=" + actor + "]";
+	}
+	public int getFilmId() {
+		return filmId;
+	}
+	public void setFilmId(int filmId) {
+		this.filmId = filmId;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getRentalRate() {
+		return rentalRate;
+	}
+	public void setRentalRate(int rentalRate) {
+		this.rentalRate = rentalRate;
+	}
+	public int getLength() {
+		return length;
+	}
+	public void setLength(int length) {
+		this.length = length;
+	}
+	public String getRating() {
+		return rating;
+	}
+	public void setRating(String rating) {
+		this.rating = rating;
+	}
+	public String getActor() {
+		return actor;
+	}
+	public void setActor(String actor) {
+		this.actor = actor;
+	}
+}
